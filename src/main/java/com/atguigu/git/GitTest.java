18c08f724ea16a47afc1bc383f3efbfa9865306b
Ryan <zhongyangwang14@gmail.com>
2021-10-10 18:50:45 +0100

    First Git Version

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author Ryan
+ * @create 2021-10-10-18:44
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
